e8e8416a95aa05c170c35f9a4b753b4cbeb9994c
628310 <matheus.kirs@gmail.com>
2018-09-19 16:32:35 -0300

    Intents

---
+++ app/src/main/java/com/example/a628310/churras3/Toto.kt
@@ -0,0 +1,12 @@
+package com.example.a628310.churras3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Toto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toto)
+    }
+}
